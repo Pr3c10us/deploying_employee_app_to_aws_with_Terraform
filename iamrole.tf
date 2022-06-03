3f6f994ce4d2af7da417a9f0db2325997b26a2b9
pr3c10us <owo.pre.eno@gmail.com>
2022-06-03 12:33:11 +0100

    aws role create

--- iamrole.tf
+++ iamrole.tf
@@ -27,8 +27,8 @@ data "aws_iam_policy" "aws_dynamodb_fullaccess" {
 
 resource "aws_iam_role_policy_attachment" "S3DynamoDBFullAccessRole_policies_attachment" {
   for_each = {
-    "key1" = "${data.aws_iam_policy.aws_dynamodb_fullaccess}"
-    "key2" = "${data.aws_iam_policy.aws_s3_fullaccess}"
+    "key1" = data.aws_iam_policy.aws_dynamodb_fullaccess
+    "key2" = data.aws_iam_policy.aws_s3_fullaccess
   }
   role = "${aws_iam_role.S3DynamoDBFullAccessRole}"
   policy_arn = each.value
