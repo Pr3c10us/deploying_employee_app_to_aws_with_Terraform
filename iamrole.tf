5b93520ba7e24ff7793c34ede7d860ada660312c
pr3c10us <owo.pre.eno@gmail.com>
2022-06-03 12:18:10 +0100

    aws role create

---
+++ iamrole.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "S3DynamoDBFullAccessRole" {
+  name = "S3DynamoDBFullAccessRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy" "aws_s3_fullaccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+data "aws_iam_policy" "aws_dynamodb_fullaccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "S3DynamoDBFullAccessRole_policies_attachment" {
+  for_each = {
+    "key1" = "${data.aws_iam_policy.aws_dynamodb_fullaccess}"
+    "key2" = "${data.aws_iam_policy.aws_s3_fullaccess}"
+  }
+  role = "${aws_iam_role.S3DynamoDBFullAccessRole}"
+  policy_arn = each.value
+}
